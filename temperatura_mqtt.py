b9a8c12285c4b33b3d6ae22aca95c3c51b1f3257
fermingp <124069712+fermingp@users.noreply.github.com>
2023-04-16 23:55:47 +0200

    temperatura

--- temperatura_mqtt.py
+++ temperatura_mqtt.py
@@ -1,51 +1,64 @@
-
-"""
-Ejercicio número 3
-"""
 from paho.mqtt.client import Client
-from time import sleep
 import sys
-
-t1_temperatures = [] #Variables comunes para realizar los calculos solicitados
-t2_temperatures = []
-
-def calculo_estadisticas():
-    t1_max = max(t1_temperatures)
-    t1_min = min(t1_temperatures)
-    t1_media = sum(t1_temperatures) / len(t1_temperatures)
-    t2_max = max(t2_temperatures)
-    t2_min = min(t2_temperatures)
-    t2_media = sum(t2_temperatures) / len(t2_temperatures)
-    print(f'SENSOR 1 - Max: {t1_max}, Min: {t1_min}, Media: {t1_media}')
-    print(f'SENSOR 2 - Max: {t2_max}, Min: {t2_min}, Media: {t2_media}')
-    # Vaciamos las listas
-    t1_temperatures.clear()
-    t2_temperatures.clear()
+import time
 
 
+def calculo_estadisticas(userdata):
+    temp = userdata["temperatures"]
+    t_min = min(temp)
+    t_max = max(temp)
+    mean = sum(temp) // len(temp) #media
+    
+    t1 = userdata["t1"]
+    t1_min = min(t1)
+    t1_max = max(t1)
+    mean1 = sum(t1) // len(t1)
+    
+    t2 = userdata["t2"]
+    t2_min = min(t2)
+    t2_max = max(t2)
+    mean2 = sum(t2) // len(t2)
+    
+    print(f'Total- Max: {t_max}, Min: {t_min}, Media: {mean}')
+    print(f'Sensor 1- Max: {t1_max}, Min: {t1_min}, Media: {mean1}')
+    print(f'Sensor 2- Max: {t2_max}, Min: {t2_min}, Media: {mean2}')
+
+    
+    
 def on_message(client, userdata, msg):
-    data = msg.payload.decode()
-    topic = msg.topic
-    if topic == 'temperature/t1':
-        t1_temperatures.append(float(data))
-    elif topic == 'temperature/t2':
-        t2_temperatures.append(float(data))
+
+    try:
+        print(msg.topic, msg.payload)
+        n =  float(msg.payload)
+        userdata["temperatures"].append(n)
+        
+        if msg.topic == 'temperature/t1':
+            userdata["t1"].append(n)
+        else:
+            userdata["t2"].append(n)
+            
+    except ValueError:
+        pass
+    except Exception as e:
+        raise e
+
 
 def main(hostname):
-    client = Client()
+    userdata = {'temperatures':[], 't1':[],'t2':[]}
+    client = Client(userdata=userdata)
     client.on_message = on_message
-    print(f'Connecting on channels numbers on {hostname}')
+    print(f'Connecting on channels temperature on {hostname}')
     client.connect(hostname)
-    client.subscribe('temperature/#')
+    client.subscribe('temperature/#') #escuchar todas las temperaturas
     client.loop_start()
-
+    
     while True:
-        sleep(5) #Esperamos 5 segundos
-        calculo_estadisticas()
+        time.sleep(5) #Esperamos 5 segundos
+        calculo_estadisticas(userdata)
 
 
-if __name__ == '__main__':
-    hostname = 'simba.fdi.ucm.es'
-    if len(sys.argv)>1:
-        hostname = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv)<2:
+        print(f"Usage: {sys.argv[0]} hostname")
+    hostname = sys.argv[1]
     main(hostname)
